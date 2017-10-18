c5cea6ff6c6cb5d925904c01770e5e3068b3d14d
Daniel Kim <danielkim713@gmail.com>
2017-10-18 08:39:30 -0700

    changed and ammended code

--- app.js
+++ app.js
@@ -2,91 +2,114 @@ let google_url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=';
 let api_key = 'AIzaSyAtDSnmVSyt7-55EYpE1milbl1KvwXrGGA';
 
 function fetchData(userInput) {
-	let body = {
-		request: {
-			passengers: {
-				kind: 'qpxexpress#passengerCounts',
-				adultCount: userInput.passengers
-			},
-			slice: [
-				{
-					kind: 'qpxexpress#sliceInput',
-					origin: userInput.origin.toUpperCase(),
-					destination: userInput.destination.toUpperCase(),
-					date: userInput.departure,
-				},
+    let body = {
+        request: {
+            passengers: {
+                kind: 'qpxexpress#passengerCounts',
+                adultCount: userInput.passengers
+            },
+            slice: [
+                {
+                    kind: 'qpxexpress#sliceInput',
+                    origin: userInput.origin.toUpperCase(),
+                    destination: userInput.destination.toUpperCase(),
+                    date: userInput.departure,
+                },
                 {
                     kind: 'qpxexpress#sliceInput',
                     origin: userInput.destination.toUpperCase(),
                     destination: userInput.origin.toUpperCase(),
                     date: userInput.arrival,
                 }
-			],
+            ],
             maxPrice: 'USD' + userInput.budget.toString()
-		}
-	};
+        }
+    };
 
-	let headers = {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json',
-	};
+    let headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    };
 
-	fetch(google_url + api_key,
-		{
-			headers: headers,
-			method: 'POST',
-			body: JSON.stringify(body)
-		}
-	).then(function(response) {
-		return response.json();
-	}).then(function(data) {
+    fetch(google_url + api_key,
+        {
+            headers: headers,
+            method: 'POST',
+            body: JSON.stringify(body)
+        }
+    ).then(function(response) {
+        return response.json();
+    }).then(function(data) {
         var results = [];
 
-        var dataArray = []
+        var tripObject = {
+            goingThere: [],
+            comingBack: [],
+            price: null
+        }
 
-        var flightObject = {
+        var flight = {
             price: null,
             flightNumber: null,
             arrivalTime: null,
             departureTime: null
         };
 
-		for (let i = 0; i <= 50; i+=10) {
+        for (let i = 0; i <= 50; i+=10) {
             console.log(i);
-            flightObject.price = data.trips.tripOption[i].saleTotal
-            for (let j = 0; j < data.trips.tripOption[i].slice[1].segment.length; j++) {
-                flightObject.flightNumber = data.trips.tripOption[i].slice[1].segment[j].flight.carrier + data.trips.tripOption[i].slice[1].segment[j].flight.number;
-                flightObject.arrivalTime = data.trips.tripOption[i].slice[1].segment[j].leg[0].arrivalTime
-                flightObject.departureTime = data.trips.tripOption[i].slice[1].segment[j].leg[0].departureTime
-                dataArray.push(flightObject);
+            tripObject.price = data.trips.tripOption[i].saleTotal
+            for (let j = 0; j < data.trips.tripOption[i].slice.length; j++) {
+                for (let k = 0; k < data.trips.tripOption[i].slice[j].segment.length; k++) {
+                    flight.flightNumber = data.trips.tripOption[i].slice[j].segment[k].flight.carrier + data.trips.tripOption[i].slice[j].segment[k].flight.number;
+                    flight.arrivalTime = data.trips.tripOption[i].slice[j].segment[k].leg[0].arrivalTime
+                    flight.departureTime = data.trips.tripOption[i].slice[j].segment[k].leg[0].departureTime
+                    if (j === 0) {
+                        tripObject.goingThere.push(flight);
+                    } else if (j === 1) {
+                        tripObject.comingBack.push(flight);
+                    } 
 
-                flightObject = {
-                    price: null,
-                    flightNumber: null,
-                    arrivalTime: null,
-                    departureTime: null
-                };
+                    flight = {
+                        price: null,
+                        flightNumber: null,
+                        arrivalTime: null,
+                        departureTime: null
+                    };
+                }
             };
-            results.push(dataArray);
-            dataArray = [];
+            results.push(tripObject);
+            tripObject = {
+                goingThere: [],
+                comingBack: [],
+                price: null
+            } 
         };
         populateResults(results);
-	}).catch(function(err) {
+    }).catch(function(err) {
         console.log(err);
     });
 }
 
 function populateResults(results) {
     for (let i = 0; i < results.length; i++) {
-        console.log(results);
         for (let j = 0; j < results[i].length; j++) {
-            let result = '<div data-key="trip' + i + 'flight' + j + '">'+ results[i][j].flightNumber + ' ' + results[i][j].price + ' ' + results[i][j].departureTime + ' ' + results[i][j].departureTime + ' </div>'
-            $('.js-search-results').append(result);
+            for (let i=0; i < goingThere.length; i++) {
+                let result = '<div data-key="trip' + i + 'flight' + j + '">'+ results[i][j].flightNumber + ' ' + results[i][j].price + ' ' + results[i][j].departureTime + ' ' + results[i][j].arrivalTime + ' </div>'
+                $('.js-search-results').append(result);
+            }
+            for (let i=0; i < comingBack.length; i++){
+                let result = '<div data-key="trip' + i + 'flight' + j + '">'+ results[i][j].flightNumber + ' ' + results[i][j].price + ' ' + results[i][j].departureTime + ' ' + results[i][j].arrivalTime + ' </div>'
+                $('.js-search-results').append(result);               
+            }
         }
     }
 }
 
 $(document).ready(function() {
+    $("button").click(function(){
+          $(".flight-intro").hide();
+    });
+
     $('#myForm').submit(function() {
         event.preventDefault();
         let values = {};
@@ -96,7 +119,6 @@ $(document).ready(function() {
         $inputs.each(function() {
             values[this.name] = $(this).val();
         });
-
         fetchData(values);
     });
 });
