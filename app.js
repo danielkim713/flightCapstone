f7a406afd89ddd63ded594b1b10e544d8bc6a276
Daniel Kim <danielkim713@gmail.com>
2017-10-20 19:28:59 -0700

    added stuff

--- app.js
+++ app.js
@@ -105,6 +105,17 @@ function populateResults(results) {
     }
 }
 
+let myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
 $(document).ready(function() {
     $("button").click(function(){
           $(".flight-intro").hide();
