c6143afcff03340e63ec0a6170f051f6634687a9
Daniel Kim <danielkim713@gmail.com>
2017-10-23 17:51:36 -0700

    last touches

---
+++ README.md
@@ -0,0 +1,11 @@
+Flight Capstone
+
+<img src="flightcapstoneSS.jpg">
+
+Summary: Application takes in parameters desired by user in searching for a flight to fit a certain budget and timeframe.  
+
+Technology used: 
+	-HTML
+	-CSS
+	-JavaScript
+	-jQuery
--- app.js
+++ app.js
@@ -105,16 +105,6 @@ function populateResults(results) {
     }
 }
 
-let myVar;
-
-function myFunction() {
-    myVar = setTimeout(showPage, 3000);
-}
-
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
-}
 
 $(document).ready(function() {
     $("button").click(function(){
